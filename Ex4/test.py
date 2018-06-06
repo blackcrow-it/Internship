8991f29caa7f40227c4d08f46e684ccd3953d65f
quanghung <blackcrow.it@gmail.com>
2018-06-07 00:25:03 +0700

    ADD index.html

--- Ex4/test.py
+++ Ex4/test.py
@@ -1,15 +1,17 @@
 from flask import Flask, request
 from redis import Redis
 redis = Redis(host='127.0.0.1', port=6379)
-app = Flask(__name__)
+app = Flask(__name__, static_folder='')
 
 @app.route("/")
 def main():
     user_agent = request.headers.get('User-Agent')
     redis.pfadd('user', user_agent)
     count_ua = redis.pfcount('user')
+    print 'Count user-agent: {}'.format(count_ua)
     print user_agent
-    return '<p>Your browser is {}!</p><p>Count user-agent: {} browser</p>'.format(user_agent, count_ua)
+    return app.send_static_file('template/index.html')
+    # return 'Hello'
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug = True)
