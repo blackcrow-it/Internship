7e9c9ffc6a548e90b4d5151ab71f98982d4d8d73
quanghung <blackcrow.it@gmail.com>
2018-06-07 00:09:10 +0700

    TEST Redis

--- Ex4/test.py
+++ Ex4/test.py
@@ -1,9 +1,15 @@
-from flask import Flask
+from flask import Flask, request
+from redis import Redis
+redis = Redis(host='127.0.0.1', port=6379)
 app = Flask(__name__)
 
 @app.route("/")
 def main():
-    return "Welcome!"
+    user_agent = request.headers.get('User-Agent')
+    redis.pfadd('user', user_agent)
+    count_ua = redis.pfcount('user')
+    print user_agent
+    return '<p>Your browser is {}!</p><p>Count user-agent: {} browser</p>'.format(user_agent, count_ua)
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host="0.0.0.0", port=5000, debug = True)
