f91b2a43122d9ee2e6f5be07cd087cb6f6d2de11
quanghung <blackcrow.it@gmail.com>
2018-06-11 15:58:09 +0700

    ADD supervisor

--- Ex4/hello.py
+++ Ex4/hello.py
@@ -1,3 +1,17 @@
-def test():
-    return 'hello world'
-print test()
+from flask import Flask, request, render_template
+from redis import Redis
+redis = Redis(host='127.0.0.1', port=6379)
+app = Flask(__name__)
+
+@app.route("/")
+def main():
+    user_agent = request.headers.get('User-Agent')
+    redis.pfadd('user', user_agent)
+    count_ua = redis.pfcount('user')
+    # print 'Count user-agent: {}'.format(count_ua)
+    # print user_agent
+    return render_template('/index.html')
+    # return 'Hello'
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5001, debug = True)
--- Ex4/test.py
+++ Ex4/test.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, render_template
 from redis import Redis
 redis = Redis(host='127.0.0.1', port=6379)
 app = Flask(__name__)
@@ -8,9 +8,9 @@ def main():
     user_agent = request.headers.get('User-Agent')
     redis.pfadd('user', user_agent)
     count_ua = redis.pfcount('user')
-    print 'Count user-agent: {}'.format(count_ua)
-    print user_agent
-    return app.send_static_file('template/index.html')
+    # print 'Count user-agent: {}'.format(count_ua)
+    # print user_agent
+    return render_template('/index.html')
     # return 'Hello'
 
 if __name__ == "__main__":
