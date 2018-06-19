a3be500a436e2e88d43ecbb502032de16f7fb512
Manic <blackcrow.it@gmail.com>
2018-06-19 16:34:31 +0700

    Count user-agent each day

--- Ex4/roles/flask/templates/main.py
+++ Ex4/roles/flask/templates/main.py
@@ -1,14 +1,17 @@
 from flask import Flask, request, render_template
 from redis import Redis
+from time import gmtime, strftime
 redis = Redis(host='127.0.0.1', port=6379)
 app = Flask(__name__)
 
 @app.route("/")
 def main():
     user_agent = request.headers.get('User-Agent')
-    redis.pfadd('user', user_agent)
+    time_now = strftime("%Y-%m-%d", gmtime())    
+    redis.pfadd(time_now, user_agent)
     count_ua = redis.pfcount('user')
-    # print 'Count user-agent: {}'.format(count_ua)
+    print time_now
+    print 'Count user-agent: {}'.format(count_ua)
     # print user_agent
     return render_template('/index.html')
     # return 'Hello'
