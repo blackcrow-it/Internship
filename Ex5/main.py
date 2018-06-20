abbaaf775d340b670084872edab50c2d3c8e05d3
Manic <blackcrow.it@gmail.com>
2018-06-20 17:29:37 +0700

    ADD ratio

---
+++ Ex5/main.py
@@ -0,0 +1,16 @@
+filelog = '/home/quanghung/Desktop/Internship/Ex5/var/log/nghttp2/access.log'
+list_ssl_protocol = []
+list_ssl_cipher = []
+with open(filelog) as f:
+    lines = f.readlines()
+    for i in lines:
+        ssl_cipher = i.split(' ')
+        list_ssl_protocol.append(ssl_cipher[3])
+        list_ssl_cipher.append(ssl_cipher[4].replace('\n',''))
+    count_ssl_cipher = len(list(set(list_ssl_cipher)))
+    count_ssl_protocol = len(list(set(list_ssl_protocol)))
+    ratio = float(count_ssl_protocol) / float(count_ssl_cipher)
+    print str(int(ratio*100))+"%"
+    # for k in list(set(ssl_cipher)):
+    #     count_sc = ssl_cipher.count(k)
+  
